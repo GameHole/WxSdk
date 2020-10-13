e88f4561fda1ea41b6cc71ccf324f4dd297328ae
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-10-13 17:52:14 +0800

    提交

---
+++ Editor/Scripts/WxBuilderHelper.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+[InitializeOnLoad]
+public class WxBuilderHelper
+{
+    //[MenuItem("WX/设置SDK参数")]
+    static void SetValues()
+    {
+        string entryJavaTxt = AssetDatabase.GUIDToAssetPath("8d0d0ee489b31744fbc356d241c89f78");
+        string andPath = AssetDatabase.GUIDToAssetPath("7fac2c4dd83f56442b8430a7d4492144");
+        if (!Directory.Exists(andPath))
+        {
+            Directory.CreateDirectory(andPath);
+        }
+        string file = File.ReadAllText(entryJavaTxt);
+        File.WriteAllText(Path.Combine(andPath, Path.GetFileName(entryJavaTxt)), file.Replace("#PACKAGE#", Application.identifier));
+        XmlSetter.SetXml();
+        GradleSetter.SetGradle();
+        AssetDatabase.Refresh();
+    }
+    [MenuItem("WX/设置SDK参数")]
+    static void SetValues_1()
+    {
+        string aarsPath = AssetDatabase.GUIDToAssetPath("7fac2c4dd83f56442b8430a7d4492144");
+        string mmPath = AssetDatabase.GUIDToAssetPath("034dd7365e62353469769ea304901074");
+        string entryJavaTxt = AssetDatabase.GUIDToAssetPath("8d0d0ee489b31744fbc356d241c89f78");
+        string pluginPath = "Assets/Plugins";
+        string andPath = Path.Combine(pluginPath+ "/Android", Path.GetFileName(aarsPath));
+        string iosPath = Path.Combine(pluginPath + "/IOS", Path.GetFileName(mmPath));
+        if (!Directory.Exists(andPath))
+        {
+            Directory.CreateDirectory(andPath);
+        }
+        string file = File.ReadAllText(entryJavaTxt);
+        File.WriteAllText(Path.Combine(andPath, Path.GetFileName(entryJavaTxt)), file.Replace("#PACKAGE#", Application.identifier));
+        CopyDirectory(aarsPath, andPath);
+        CopyDirectory(mmPath, iosPath);
+        XmlSetter.SetXml();
+        GradleSetter.SetGradle();
+        AssetDatabase.Refresh();
+    }
+    static void CopyDirectory(string sourceDir, string destDir)
+    {
+        if (!Directory.Exists(destDir))
+        {
+            Directory.CreateDirectory(destDir);
+        }
+
+        try
+        {
+            Debug.Log(sourceDir);
+            string[] fileList = Directory.GetFiles(sourceDir, "*");
+            foreach (string f in fileList)
+            {
+                // Remove path from the file name.
+                string fName = f.Substring(sourceDir.Length + 1);
+                if (fName.Contains(".meta")) continue;
+                // Use the Path.Combine method to safely append the file name to the path.
+                // Will overwrite if the destination file already exists.
+                File.Copy(Path.Combine(sourceDir, fName), Path.Combine(destDir, fName), true);
+            }
+        }
+
+        catch (DirectoryNotFoundException dirNotFound)
+        {
+            throw new DirectoryNotFoundException(dirNotFound.Message);
+        }
+    }
+}
---
+++ Editor/Scripts/XmlSetter.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using UnityEditor;
+using System.IO;
+public static class XmlSetter 
+{
+    //[MenuItem("Wx/TExt")]
+    public static void SetXml()
+    {
+        XmlDocument document = new XmlDocument();
+        string plgpath = "Assets/Plugins/Android";
+        string plugpath = $"{plgpath}/AndroidManifest.xml";
+        if (!File.Exists(plugpath))
+        {
+            string con = File.ReadAllText(AssetDatabase.GUIDToAssetPath("8fd79680135600b4392dcdae706b5f12"));
+            if (!Directory.Exists(plgpath))
+                Directory.CreateDirectory(plgpath);
+            File.WriteAllText(plugpath, con);
+        }
+        document.Load(plugpath);
+        var settedNode = FindNode(document, "/manifest/application/activity", "android:name", $"{Application.identifier}.wxapi.WXEntryActivity");
+        if (settedNode != null) return;
+        var node = FindNode(document, "/manifest/application/activity", "android:name", "package.wxapi.WXEntryActivity");
+        if (node != null)
+        {
+            node.Attributes["android:name"].Value = $"{Application.identifier}.wxapi.WXEntryActivity";
+            node.Attributes["android:taskAffinity"].Value = Application.identifier;
+        }
+        else
+        {
+            var appNode = document.SelectSingleNode("/manifest/application");
+            var em = document.CreateElement("activity");
+            em.Attributes.Append(em.CreateAttribute("name", $"{Application.identifier}.wxapi.WXEntryActivity"));
+            em.Attributes.Append(em.CreateAttribute("taskAffinity", Application.identifier));
+            em.Attributes.Append(em.CreateAttribute("label",  "@string/app_name"));
+            em.Attributes.Append(em.CreateAttribute("theme",  "@android:style/Theme.Translucent.NoTitleBar"));
+            em.Attributes.Append(em.CreateAttribute("exported",  "true"));
+            em.Attributes.Append(em.CreateAttribute("launchMode",  "singleTask"));
+            appNode.AppendChild(em);
+            SetPermission(document, "android.permission.INTERNET");
+            SetPermission(document, "android.permission.ACCESS_WIFI_STATE");
+            SetPermission(document, "android.permission.ACCESS_NETWORK_STATE");
+        }
+        document.Save(plugpath);
+        //AssetDatabase.Refresh();
+    }
+    static void SetPermission(XmlDocument xdoc,string value)
+    {
+        var node = FindNode(xdoc, "/manifest/uses-permission", "android:name", value);
+        if (node == null)
+        {
+            var manifestNode = xdoc.SelectSingleNode("/manifest");
+            var ele = xdoc.CreateElement("uses-permission");
+            ele.Attributes.Append(ele.CreateAttribute("name", value));
+            manifestNode.AppendChild(ele);
+        }
+    }
+    public static XmlAttribute CreateAttribute(this XmlNode node, string attributeName, string value)
+    {
+        try
+        {
+            XmlDocument doc = node.OwnerDocument;
+            XmlAttribute attr = null;
+            
+            attr = doc.CreateAttribute(attributeName, "http://schemas.android.com/apk/res/android");
+            attr.Value = value;
+            node.Attributes.SetNamedItem(attr);
+            return attr;
+        }
+        catch (System.Exception err)
+        {
+            string desc = err.Message;
+            return null;
+        }
+    }
+    static XmlNode FindNode(XmlDocument xmlDoc, string xpath, string attributeName, string attributeValue)
+    {
+        XmlNodeList nodes = xmlDoc.SelectNodes(xpath);
+        //Debug.Log(nodes.Count);
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            XmlNode node = nodes.Item(i);
+            var att = node.Attributes[attributeName];
+            if (att != null)
+            {
+                if (att.Value == attributeValue)
+                {
+                    return node;
+                }
+            }
+        }
+        return null;
+    }
+}
