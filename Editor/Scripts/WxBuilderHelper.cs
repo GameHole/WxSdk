82edd0baeca3f14b8aa2235fa02abba0161c0b78
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-10-14 11:11:52 +0800

    修改BUG

--- Editor/Scripts/WxBuilderHelper.cs
+++ Editor/Scripts/WxBuilderHelper.cs
@@ -26,7 +26,7 @@ public class WxBuilderHelper
     {
         string aarsPath = AssetDatabase.GUIDToAssetPath("7fac2c4dd83f56442b8430a7d4492144");
         string mmPath = AssetDatabase.GUIDToAssetPath("034dd7365e62353469769ea304901074");
-        string entryJavaTxt = AssetDatabase.GUIDToAssetPath("8d0d0ee489b31744fbc356d241c89f78");
+        string entryJavaTxt = AssetDatabase.GUIDToAssetPath("e2544a5d6d6c72c4ba194a7f31faf56e");
         string pluginPath = "Assets/Plugins";
         string andPath = Path.Combine(pluginPath+ "/Android", Path.GetFileName(aarsPath));
         string iosPath = Path.Combine(pluginPath + "/IOS", Path.GetFileName(mmPath));
@@ -35,7 +35,7 @@ public class WxBuilderHelper
             Directory.CreateDirectory(andPath);
         }
         string file = File.ReadAllText(entryJavaTxt);
-        File.WriteAllText(Path.Combine(pluginPath + "/Android", Path.GetFileName(entryJavaTxt)), file.Replace("#PACKAGE#", Application.identifier));
+        File.WriteAllText(Path.Combine(pluginPath + "/Android", Path.GetFileNameWithoutExtension(entryJavaTxt)+".java"), file.Replace("#PACKAGE#", Application.identifier));
         CopyDirectory(aarsPath, andPath);
         CopyDirectory(mmPath, iosPath);
         XmlSetter.SetXml();
