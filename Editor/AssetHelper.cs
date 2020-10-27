fdf213c5312c0a81e3b7f023ec97d14c9cd583c5
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-10-27 11:36:41 +0800

    修改结构

---
+++ Editor/AssetHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+public static class AssetHelper
+{
+    public static T CreateAsset<T>() where T : AScriptableObject
+    {
+        var res = ScriptableObject.CreateInstance<T>();
+        string path = "Assets/Resources/" + res.filePath+".asset";
+        if (File.Exists(path))
+        {
+            return Resources.Load<T>(path);
+        }
+        string dir = Path.GetDirectoryName(path);
+        if (!Directory.Exists(dir))
+            Directory.CreateDirectory(dir);
+        AssetDatabase.CreateAsset(res, path);
+        return res;
+    }
+    public static void Save(this ScriptableObject sc)
+    {
+        EditorUtility.SetDirty(sc);
+        AssetDatabase.SaveAssets();
+    }
+}
